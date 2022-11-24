e30b0ad84539675c8db841f21b2a171adbc08dde
Dan Popov <akiowebdev@gmail.com>
2022-11-25 08:33:00 +0900

    added account detail, create order

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ def get_all_accounts():
     url_path="/api/v3/brokerage/accounts"
     body=""
     message = timestamp + method + url_path + body;
-    print(message)
     signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
-    print(signature.hex(), timestamp)
     url = "https://coinbase.com/api/v3/brokerage/accounts"
     headers = {
         "accept": "application/json",
         "CB-ACCESS-KEY": ACCESS_KEY,
-        "CB-ACCESS-SIGN": signature.hex(),
+        "CB-ACCESS-SIGN": signature,
         "CB-ACCESS-TIMESTAMP": timestamp
     }
 
@@ -27,10 +25,43 @@ def get_all_accounts():
 def get_account_detail(uuid):
     timestamp = str(int(time.time())) 
     method="GET"
-    url="/api/v3/brokerage/accounts"
-    message = timestamp + method + url
+    url_path="/api/v3/brokerage/accounts/"+uuid
+    body=""
+    message = timestamp + method + url_path + body;
     signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
-    print(signature.hex(), timestamp)
+    url = "https://coinbase.com/api/v3/brokerage/accounts/"+uuid
+    headers = {
+        "accept": "application/json",
+        "CB-ACCESS-KEY": ACCESS_KEY,
+        "CB-ACCESS-SIGN": signature,
+        "CB-ACCESS-TIMESTAMP": timestamp
+    }
 
+    response = requests.get(url, headers=headers)
+    print(response.text)
+
+def create_order(client_order_id, product_id, side, order_configuration):
+    timestamp = str(int(time.time())) 
+    method="POST"
+    url_path="/api/v3/brokerage/orders"
+    body={
+        "client_order_id": client_order_id,
+        "product_id": product_id,
+        "side": side,
+        "order_configuration": order_configuration
+    }
+    message = timestamp + method + url_path + json.dumps(body);
+    signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    url = "https://coinbase.com/api/v3/brokerage/orders"
+    headers = {
+        "accept": "application/json",
+        "CB-ACCESS-KEY": ACCESS_KEY,
+        "CB-ACCESS-SIGN": signature,
+        "CB-ACCESS-TIMESTAMP": timestamp,
+        "content-type": "application/json"
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    print(response.text)
 
 get_all_accounts()
