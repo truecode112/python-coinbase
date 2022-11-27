77ff3a5a4075a9018165ddead1629a88615b3951
Dan Popov <akiowebdev@gmail.com>
2022-11-28 00:08:25 +0900

    Changed  header

--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import json, hmac, hashlib, time, base64
 
 ACCESS_KEY = "L3drKMRZ1BFYHp8U"
 API_SECRET = "1Ikj9rdRSeGTEE5J3y3OPsDYV4PKUYmh"
+PASS_PHARSE = "8bEs4!yn%rG#VTK"
 
 def get_all_accounts():
-    timestamp = str(int(time.time())) 
+    timestamp = str(time.time()) 
     method="GET"
     url_path="/api/v3/brokerage/accounts"
     body=""
     message = timestamp + method + url_path + body;
-    signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    hmac_key = base64.b64decode(API_SECRET);
+    signature = hmac.new(hmac_key, message.encode('utf-8'), digestmod=hashlib.sha256)
+    signature_b64 = base64.b64encode(signature.digest());
+    #signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
     url = "https://coinbase.com/api/v3/brokerage/accounts"
     headers = {
         "accept": "application/json",
+        "CB-VERSION": "2022-11-24",
         "CB-ACCESS-KEY": ACCESS_KEY,
-        "CB-ACCESS-SIGN": signature,
-        "CB-ACCESS-TIMESTAMP": timestamp
+        "CB-ACCESS-SIGN": signature_b64,
+        "CB-ACCESS-TIMESTAMP": timestamp,
+        "CB-ACCESS-PASSPHRASE": PASS_PHARSE,
+        "Content-type": 'application/json'
     }
 
     response = requests.get(url, headers=headers)
