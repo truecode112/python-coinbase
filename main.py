beda508d40abad3136c0a641162582019caccc48
Dan Popov <akiowebdev@gmail.com>
2022-11-28 13:43:41 +0900

    changed CB-ACCESS-SIGN

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import requests
+from requests.auth import AuthBase
 import json, hmac, hashlib, time, base64
 
 ACCESS_KEY = "L3drKMRZ1BFYHp8U"
 API_SECRET = "1Ikj9rdRSeGTEE5J3y3OPsDYV4PKUYmh"
-PASS_PHARSE = "8bEs4!yn%rG#VTK"
 
 def get_all_accounts():
-    timestamp = str(time.time()) 
+    timestamp = str(int(time.time())) 
     method="GET"
     url_path="/api/v3/brokerage/accounts"
     body=""
     message = timestamp + method + url_path + body;
-    hmac_key = base64.b64decode(API_SECRET);
-    signature = hmac.new(hmac_key, message.encode('utf-8'), digestmod=hashlib.sha256)
-    signature_b64 = base64.b64encode(signature.digest());
-    #signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    #hmac_key = base64.b64decode(API_SECRET);
+    #signature = hmac.new(hmac_key, message.encode('utf-8'), digestmod=hashlib.sha256)
+    #signature_b64 = base64.b64encode(signature.digest());
+    signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    print(signature.hex())
     url = "https://coinbase.com/api/v3/brokerage/accounts"
     headers = {
         "accept": "application/json",
-        "CB-VERSION": "2022-11-24",
         "CB-ACCESS-KEY": ACCESS_KEY,
-        "CB-ACCESS-SIGN": signature_b64,
-        "CB-ACCESS-TIMESTAMP": timestamp,
-        "CB-ACCESS-PASSPHRASE": PASS_PHARSE,
-        "Content-type": 'application/json'
+        "CB-ACCESS-SIGN": signature.hex(),
+        "CB-ACCESS-TIMESTAMP": timestamp
     }
 
     response = requests.get(url, headers=headers)
+    print(response)
     print(response.text)
 
 def get_account_detail(uuid):
@@ -71,4 +70,27 @@ def create_order(client_order_id, product_id, side, order_configuration):
     response = requests.post(url, json=body, headers=headers)
     print(response.text)
 
-get_all_accounts()
+def get_all_products():
+    timestamp = str(int(time.time())) 
+    method="GET"
+    url_path="/api/v3/brokerage/products"
+    body=""
+    message = timestamp + method + url_path + body;
+    #hmac_key = base64.b64decode(API_SECRET);
+    #signature = hmac.new(hmac_key, message.encode('utf-8'), digestmod=hashlib.sha256)
+    #signature_b64 = base64.b64encode(signature.digest());
+    signature = hmac.new(API_SECRET.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest()
+    print(signature.hex())
+    url = "https://coinbase.com/api/v3/brokerage/products"
+    headers = {
+        "accept": "application/json",
+        "CB-ACCESS-KEY": ACCESS_KEY,
+        "CB-ACCESS-SIGN": signature.hex(),
+        "CB-ACCESS-TIMESTAMP": timestamp
+    }
+
+    response = requests.get(url, headers=headers)
+    print(response)
+    print(response.text)
+
+get_all_products()
